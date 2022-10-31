6570afa35c943f6ee8cd58038c591857af4ac29c
Deverty <bychenkov.a.k@edu.mirea.ru>
2022-10-31 15:33:07 +0300

    profile page design

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { GrFormClose } from "react-icons/gr";
 import { VscDebugBreakpointLogUnverified } from "react-icons/vsc";
 import UserContext from "../../context/UserContext";
 import styles from "./ProfilePage.module.css";
@@ -7,22 +8,26 @@ const ProfilePage = () => {
   const { user } = useContext(UserContext);
   return (
     <div className={styles.profile_div}>
+      <h1>Личный кабинет</h1>
       <h2>{user?.phone}</h2>
-      <h2 className={styles.bonus_points}>
-        {user?.bonus_points}{" "}
+      <h3 className={styles.bonus_points}>
+        Бонусные баллы: &nbsp;{user?.bonus_points}&nbsp;
         <VscDebugBreakpointLogUnverified></VscDebugBreakpointLogUnverified>
-      </h2>
-      <h3>Сохранённые адреса:</h3>
+      </h3>
+      <div>
+        <h3>Потратить бонусы:</h3>
+      </div>
+      <div>
+        <h3>Ваши заказы:</h3>
+      </div>
       <div className={styles.addresses_div}>
+        <h3>Сохранённые адреса:</h3>
         {user?.saved_addresses.map((saved_address) => (
-          <span key={saved_address.name}>{saved_address.name}</span>
+          <div key={saved_address.name} className={styles.address}>
+            <span>{saved_address.name}</span>
+          </div>
         ))}
         <button>Добавить адрес</button>
-      </div>
-      <h3>Ваши заказы:</h3>
-      <h3>Потратить бонусы:</h3>
-      <div>
-        
       </div>
       <a href="/" onClick={() => localStorage.setItem("token", "")}>
         Выйти
